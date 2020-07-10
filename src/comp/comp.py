6f38c57006c94948594ce760f8c061eee0c7898e
Angelg10 <angelguzman97@gmail.com>
2020-07-10 13:01:11 -0400

    cityreader, comp

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -57,7 +57,7 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [tuple(humans.name + ', ' + str(humans.age) for humans in humans if 27 <= humans.age <= 32)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
@@ -66,7 +66,7 @@ print(f)
 print("All names uppercase:")
 g = [humans.name.upper() + '-' + str(humans.age + 5) for humans in humans]
 print(g)
-print(humans)
+# print(humans)
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
